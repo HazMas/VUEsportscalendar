bd4c9217ba03a459b7988b21824bd1183c280ff0
Santiago M.A <santiagomartinagra@gmail.com>
2018-02-04 20:12:51 +0100

    feat(API-LVP): Llamadas sobre https

--- src/api/lvp.js
+++ src/api/lvp.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = 'http://pro.lvp.es/api/superliga/lol/temporada/'
+const API_BASE = 'https://pro.lvp.es/api/superliga/lol/temporada/'
 
 export default {
   getMatches () {
