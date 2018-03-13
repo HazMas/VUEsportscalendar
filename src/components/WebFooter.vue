17adca4e0aa66f9234cb70cfee568f5d8473373e
Santiago M.A <santiagomartinagra@gmail.com>
2018-03-13 18:02:28 +0100

    feat(App): Creado componente footer
    Closes #33

---
+++ src/components/WebFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <a href="https://www.behance.net/alvaroherrerogarcia">
+      Diseñado
+    </a>
+    &
+    <a href="https://twitter.com/SantiMA10">
+      desarrollado
+    </a>
+    con 👾desde Asturias
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'web-footer'
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  color: white;
+  text-align: center;
+  & a {
+    text-decoration: none;
+    color: white
+  }
+}
+</style>
+
