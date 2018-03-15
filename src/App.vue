d4b6d7db8ee6f4e0f3d4951e1ebe8e646f083735
Álvaro <alvaro.herrero.creativo@gmail.com>
2018-03-15 22:42:07 +0100

    feat(App): añadido dinosaurio offline

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
     <header>
+      <span></span>
       <h1>
-        VUE-Sports Calendar
+        E-Sports Calendar
       </h1>
-        <detect-network>
-          <div slot="online">
-            [online] ✔️
-          </div>
-          <div slot="offline">
-            [Offine] 🚫
-          </div>
-        </detect-network>
+      <detect-network class="offline-dino">
+        <div slot="offline">
+          <img src="static/img/icons/dino.svg" alt="Offline dinosaur">
+        </div>
+      </detect-network>
     </header>
     <main>
       <router-view></router-view>
@@ -35,23 +33,36 @@ export default {
 </script>
 
 <style lang="scss">
-  button {
-    background: none;
-    color: inherit;
-    border: none;
-    padding: 0;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
-  }
-  #app {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-  .web-footer {
-    margin-top: auto;
-    padding-top: 16px;
-    padding-bottom: 16px;
-  }
+header {
+display: grid;
+grid-template-columns: 1fr 3fr 1fr;
+padding:0 6% 0 6%;
+}
+h1 {
+  text-align: center;
+}
+.offline-dino {
+  display: flex;
+  justify-content:flex-end;
+  align-items: center;
+}
+button {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.web-footer {
+  margin-top: auto;
+  padding-top: 16px;
+  padding-bottom: 16px;
+}
 </style>
