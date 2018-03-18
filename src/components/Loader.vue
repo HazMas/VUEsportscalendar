9f20469bc0f43e7c857bcafaba4ed88b9738b1e7
Santiago M.A <santiagomartinagra@gmail.com>
2018-03-18 11:09:03 +0100

    feat(CalendarView): Añadido loader
    Closes #42

---
+++ src/components/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="loader">
+    <pacman-loader></pacman-loader>
+  </div>
+</template>
+
+<script>
+import 'vue-loaders/dist/vue-loaders.css'
+import { PacmanLoader } from 'vue-loaders'
+
+export default {
+  name: 'loader',
+  components: {
+    PacmanLoader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  margin-top: 20px;
+  text-align: center;
+}
+</style>
