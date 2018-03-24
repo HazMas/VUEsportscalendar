644980d02707bae5803704ebff072e2df64228ae
Santiago M.A <santiagomartinagra@gmail.com>
2018-03-24 18:16:52 +0100

    feat(MatchView): Se cargan los datos del partido desde el API

--- src/view/MatchView.vue
+++ src/view/MatchView.vue
@@ -1,65 +1,83 @@
 <template>
   <div>
-    <h1 class="match-view">
-      {{match | startFullDate}}
-    </h1>
-    <div class="match-view__teams">
-      <div class="match-view__team">
-        <img class="match-view__team-shield-img" v-lazy="match.team_a.image_url" :alt="match.team_a.name">
-      </div>
-      <div class="match-view__result" v-if="isFinished(match) || isLive(match)">
-        <div class="match-view__result-a">
-          {{match.result_a}}
+    <loader v-if="loading">
+    </loader>
+    <div v-else>
+      <h1 class="match-view">
+        {{match | startFullDate}}
+      </h1>
+      <div class="match-view__teams">
+        <div class="match-view__team">
+          <img class="match-view__team-shield-img" v-lazy="match.team_a.image_url" :alt="match.team_a.name">
         </div>
-        <span class="match-view__separator">
-          vs
-        </span>
-        <div class="match-view__result-b">
-          {{match.result_b}}
+        <div class="match-view__result" v-if="isFinished(match) || isLive(match)">
+          <div class="match-view__result-a">
+            {{match.result_a}}
+          </div>
+          <span class="match-view__separator">
+            vs
+          </span>
+          <div class="match-view__result-b">
+            {{match.result_b}}
+          </div>
+          <span v-if="isLive(match)">
+            En juego
+          </span>
         </div>
-        <span v-if="isLive(match)">
-          En juego
+        <div class="match-view__result" v-if="isScheduled(match)">
+          {{match | startTime}} h
+        </div>
+        <div class="match-view__team">
+          <img class="match-view__team-shield-img" v-lazy="match.team_b.image_url" :alt="match.team_b.name">
+        </div>
+      </div>
+      <div class="match-view__competition-info">
+        <span class="match-view__event-competition">
+          <img :src="'/static/img/leagues/' + match.competition + '.png'" :alt="match.competition">
+        </span>
+        <span class="match-view__event-game">
+          <img :src="'/static/img/games/' + match.game + '.png'" :alt="match.game">
+        </span>
+        <span :class="matchItemEventRoundClasses">
+          J{{match.round}}
         </span>
       </div>
-      <div class="match-view__result" v-if="isScheduled(match)">
-        {{match | startTime}} h
+      <div class="match-view__live" v-if="!isFinished(match) && match.live">
+        <h2>
+          Donde ver
+        </h2>
+        <a class="match-view__live-info" v-for="live in match.live" :key="live.url" :href="live.url">
+          <img :src="'/static/img/live/' + live.platform + '.svg'" :alt="live.platform">
+        </a>
       </div>
-      <div class="match-view__team">
-        <img class="match-view__team-shield-img" v-lazy="match.team_b.image_url" :alt="match.team_b.name">
-      </div>
-    </div>
-    <div class="match-view__competition-info">
-      <span class="match-view__event-competition">
-        <img :src="'/static/img/leagues/' + match.competition + '.png'" :alt="match.competition">
-      </span>
-      <span class="match-view__event-game">
-        <img :src="'/static/img/games/' + match.game + '.png'" :alt="match.game">
-      </span>
-      <span :class="matchItemEventRoundClasses">
-        J{{match.round}}
-      </span>
-    </div>
-    <div class="match-view__live" v-if="!isFinished(match) && match.live">
-      <h2>
-        Donde ver
-      </h2>
-      <a class="match-view__live-info" v-for="live in match.live" :key="live.url" :href="live.url">
-        <img :src="'/static/img/live/' + live.platform + '.svg'" :alt="live.platform">
-      </a>
     </div>
   </div>
 </template>
 
 <script>
+import lvp from '@/api/lvp'
+import Loader from '@/components/Loader'
 import {isScheduled, isFinished, isLive, startTime, startFullDate} from '@/helpers/MatchHelpers'
 
 export default {
   name: 'match-view',
   props: ['competition', 'game', 'matchId'],
+  created () {
+    if (this.competition === 'superliga-orange') {
+      lvp.getMatch(this.game, this.matchId)
+        .then((match) => {
+          this.match = match
+          this.loading = false
+        })
+    }
+  },
+  data () {
+    return {
+      loading: true,
+      match: undefined
+    }
+  },
   computed: {
-    match () {
-      return this.$store.getters.getMatchByCompetitonGameAndId(this.competition, this.game, this.matchId)
-    },
     matchItemEventRoundClasses () {
       return {
         'match-view__event-round': true,
@@ -77,6 +95,9 @@ export default {
   filters: {
     startTime,
     startFullDate
+  },
+  components: {
+    Loader
   }
 }
 </script>
