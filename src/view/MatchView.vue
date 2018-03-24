dda165e330c9185e43511e27e4ecc854e49a1521
Santiago M.A <santiagomartinagra@gmail.com>
2018-03-24 16:40:22 +0100

    feat(MatchView): Se muestra la información de la competición

--- src/view/MatchView.vue
+++ src/view/MatchView.vue
@@ -12,7 +12,7 @@
           {{match.result_a}}
         </div>
         <span class="match-view__separator">
-          ·
+          vs
         </span>
         <div class="match-view__result-b">
           {{match.result_b}}
@@ -28,8 +28,16 @@
         <img class="match-view__team-shield-img" v-lazy="match.team_b.image_url" :alt="match.team_b.name">
       </div>
     </div>
-    <div>
-      {{match}}
+    <div class="match-view__competition-info">
+      <span class="match-view__event-competition">
+        <img :src="'/static/img/leagues/' + match.competition + '.png'" :alt="match.competition">
+      </span>
+      <span class="match-view__event-game">
+        <img :src="'/static/img/games/' + match.game + '.png'" :alt="match.game">
+      </span>
+      <span :class="matchItemEventRoundClasses">
+        J{{match.round}}
+      </span>
     </div>
   </div>
 </template>
@@ -43,6 +51,14 @@ export default {
   computed: {
     match () {
       return this.$store.getters.getMatchByCompetitonGameAndId(this.competition, this.game, this.matchId)
+    },
+    matchItemEventRoundClasses () {
+      return {
+        'match-view__event-round': true,
+        'match-view__event-round--clash': this.match.game === 'clash',
+        'match-view__event-round--lol': this.match.game === 'lol',
+        'match-view__event-round--csgo': this.match.game === 'csgo'
+      }
     }
   },
   methods: {
@@ -58,8 +74,46 @@ export default {
 </script>
 
 <style lang="scss">
+.match-view__teams {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.match-view__competition-info {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.match-view__team {
+  display: inline-block;
+}
+
+.match-view__result {
+  display: inline-block;
+}
+
+.match-view__event-round {
+  font-size: 48px;
+  font-weight: bold;
+  color: #fff;
+  padding: 3px 5px;
+  border-radius: 3px;
+  white-space: nowrap;
+} 
+
+.match-view__event-round--clash {
+  background-color: #5185E0;
+}
+
+.match-view__event-round--lol {
+  background-color: #05D4BE;
+}
+
+.match-view__event-round--csgo {
+  background-color: #FFAD63;
+}
 
 </style>
 
