8f9891d409b23443bee8973e604945e02d910600
Álvaro <alvaro.herrero.creativo@gmail.com>
2018-03-27 18:44:32 +0200

    feat(Ladders): estilado básico

--- src/components/Ladders.vue
+++ src/components/Ladders.vue
@@ -4,14 +4,41 @@
     </loader>
     <div class="ladders" v-else>
       <router-link tag="div" :to="{name: 'team-view', params: {competition: competition, game: game, teamId: ladder.team.id}}" class="ladder-item" v-for="ladder in ladders.info" :key="ladder.team.id">
-        {{ladder.rank}}
-        <img class="ladders__shield-img" v-lazy="ladder.team.image_url" :alt="ladder.team.name">
-        {{ladder.team.name}}
-        {{ladder.win}}
-        {{ladder.loss}}
-        {{ladder.draw}}
-        {{ladder.streak}}
-        {{ladder.points}}
+        <span class="ladder-team">
+          {{ladder.rank}}
+          <img class="ladders__shield-img" v-lazy="ladder.team.image_url" :alt="ladder.team.name">
+          {{ladder.team.name}}
+        </span>
+        <span class="ladder-data">
+          <span class="ladder-data-title">
+            Ganados
+          </span>
+          {{ladder.win}}
+        </span>
+        <span class="ladder-data">
+          <span class="ladder-data-title">
+            Perdidos
+          </span>
+          {{ladder.loss}}
+        </span>
+        <span class="ladder-data">
+          <span class="ladder-data-title">
+            Empates
+          </span>
+          {{ladder.draw}}
+        </span>
+        <!-- <span class="ladder-data">
+          <span class="ladder-data-title">
+            Racha
+          </span>
+          {{ladder.streak}}
+        </span> -->
+        <span class="ladder-data">
+          <span class="ladder-data-title">
+            Puntos
+          </span>
+          {{ladder.points}}
+        </span>
       </router-link>
     </div>
   </div>
@@ -61,5 +88,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.ladders {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+.ladder-item {
+  cursor: pointer;
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 6%;
+  padding: 7px ;
+  border-bottom: 1px solid rgba(255,255,255,.2);
+  &:hover {
+    background-color: rgba(255,255,255,0.1);
+  }
+}
+.ladder-team {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.ladder-data {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.ladder-data-title {
+  font-size: 10px;
+}
+.ladders__shield-img {
+  height: 50px;
+  margin: 0 15px;
+}
 
 </style>
