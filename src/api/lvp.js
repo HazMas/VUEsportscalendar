371f97387f041778aa9c800d728dbab116dd5620
Santiago M.A <santiagomartinagra@gmail.com>
2018-04-19 21:39:54 +0200

    fix(lvp): no se cargan los partidos

--- src/api/lvp.js
+++ src/api/lvp.js
@@ -5,37 +5,38 @@ const API_BASE = 'https://esportscalendar.herokuapp.com/superliga/'
 
 export default {
   getMatches () {
-    const promises = [
-      axios.get(API_BASE + 'lol/temporada/matches'),
-      axios.get(API_BASE + 'clash/temporada/matches'),
-      axios.get(API_BASE + 'csgo/temporada/matches')
-      // axios.get(API_BASE + 'lol/copa/matches'),
-      // axios.get(API_BASE + 'lolsegunda/temporada/matches')
-    ]
-    return Promise.all(promises)
-      .then((responses) => {
-        const matches = [
-          ...responses[0].data.map((match) => {
-            match.game = 'lol'
-            match.competition = 'superliga-orange'
-            match.live = this.getLive(match.game)
-            return match
-          }),
-          ...responses[1].data.map((match) => {
-            match.game = 'clash'
-            match.competition = 'superliga-orange'
-            match.live = this.getLive(match.game)
-            return match
-          }),
-          ...responses[2].data.map((match) => {
-            match.game = 'csgo'
-            match.competition = 'superliga-orange'
-            match.live = this.getLive(match.game)
-            return match
-          })
-        ]
-        return matches
-      })
+    // const promises = [
+    //   axios.get(API_BASE + 'lol/temporada/matches'),
+    //   axios.get(API_BASE + 'clash/temporada/matches'),
+    //   axios.get(API_BASE + 'csgo/temporada/matches')
+    //   // axios.get(API_BASE + 'lol/copa/matches'),
+    //   // axios.get(API_BASE + 'lolsegunda/temporada/matches')
+    // ]
+    // return Promise.all(promises)
+    //   .then((responses) => {
+    //     const matches = [
+    //       ...responses[0].data.map((match) => {
+    //         match.game = 'lol'
+    //         match.competition = 'superliga-orange'
+    //         match.live = this.getLive(match.game)
+    //         return match
+    //       }),
+    //       ...responses[1].data.map((match) => {
+    //         match.game = 'clash'
+    //         match.competition = 'superliga-orange'
+    //         match.live = this.getLive(match.game)
+    //         return match
+    //       }),
+    //       ...responses[2].data.map((match) => {
+    //         match.game = 'csgo'
+    //         match.competition = 'superliga-orange'
+    //         match.live = this.getLive(match.game)
+    //         return match
+    //       })
+    //     ]
+    //     return matches
+    //   })
+    return []
   },
   getLive (game) {
     if (game === 'csgo') {
